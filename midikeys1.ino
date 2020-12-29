d081c9af8291fd411114cf64b0c43b496847e5c9
Jonathan Rosser <jonathan.rosser@bbc.co.uk>
2020-12-29 11:45:56 +0000

    Add second keyboard

--- midikeys1.ino
+++ midikeys1.ino
@@ -1,5 +1,7 @@
 #include "MIDIUSB.h"
 
+#define D3 4
+#define D2 3
 #define D1 13
 #define D0 12
 
@@ -13,41 +15,62 @@
 #define S0 5
 
 unsigned char n;
+unsigned char x;
 unsigned char keys0[64];
 unsigned char lastkeys0[64];
+unsigned char keys1[64];
+unsigned char lastkeys1[64];
 
 void read8() {
   digitalWrite(S2, LOW); digitalWrite(S1, LOW); digitalWrite(S0, LOW);
   keys0[n+32] = digitalRead(D1);
-  keys0[n++] = digitalRead(D0);
+  keys0[n] = digitalRead(D0);
+  keys1[n+32] = digitalRead(D3);
+  keys1[n++] = digitalRead(D2);
   digitalWrite(S2, LOW); digitalWrite(S1, LOW); digitalWrite(S0, HIGH);
   keys0[n+32] = digitalRead(D1);
-  keys0[n++] = digitalRead(D0);
+  keys0[n] = digitalRead(D0);
+  keys1[n+32] = digitalRead(D3);
+  keys1[n++] = digitalRead(D2);
   digitalWrite(S2, LOW); digitalWrite(S1, HIGH); digitalWrite(S0, LOW);
   keys0[n+32] = digitalRead(D1);
-  keys0[n++] = digitalRead(D0);
+  keys0[n] = digitalRead(D0);
+  keys1[n+32] = digitalRead(D3);
+  keys1[n++] = digitalRead(D2);
   digitalWrite(S2, LOW); digitalWrite(S1, HIGH); digitalWrite(S0, HIGH);
   keys0[n+32] = digitalRead(D1);
-  keys0[n++] = digitalRead(D0);
+  keys0[n] = digitalRead(D0);
+  keys1[n+32] = digitalRead(D3);
+  keys1[n++] = digitalRead(D2);
   digitalWrite(S2, HIGH); digitalWrite(S1, LOW); digitalWrite(S0, LOW);
   keys0[n+32] = digitalRead(D1);
-  keys0[n++] = digitalRead(D0);
+  keys0[n] = digitalRead(D0);
+  keys1[n+32] = digitalRead(D3);
+  keys1[n++] = digitalRead(D2);
   digitalWrite(S2, HIGH); digitalWrite(S1, LOW); digitalWrite(S0, HIGH);
   keys0[n+32] = digitalRead(D1);
-  keys0[n++] = digitalRead(D0);
+  keys0[n] = digitalRead(D0);
+  keys1[n+32] = digitalRead(D3);
+  keys1[n++] = digitalRead(D2);
   digitalWrite(S2, HIGH); digitalWrite(S1, HIGH); digitalWrite(S0, LOW);
   keys0[n+32] = digitalRead(D1);
-  keys0[n++] = digitalRead(D0);
+  keys0[n] = digitalRead(D0);
+  keys1[n+32] = digitalRead(D3);
+  keys1[n++] = digitalRead(D2);
   digitalWrite(S2, HIGH); digitalWrite(S1, HIGH); digitalWrite(S0, HIGH);
   keys0[n+32] = digitalRead(D1);
-  keys0[n++] = digitalRead(D0);
+  keys0[n] = digitalRead(D0);
+  keys1[n+32] = digitalRead(D3);
+  keys1[n++] = digitalRead(D2);
 }
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
-  pinMode(D0, INPUT_PULLUP);  // d4-6  Data out of 74HC251 4-6 paralleled
+  pinMode(D0, INPUT_PULLUP);  // d4-6  Data out of 74HC251 4-6 paralleled - keyboard 0
   pinMode(D1, INPUT_PULLUP);  // d0-3  Data out of 74HC251 0-3 paralleled
+  pinMode(D2, INPUT_PULLUP);  // d4-6  Data out of 74HC251 4-6 paralleled - keyboard 1
+  pinMode(D3, INPUT_PULLUP);  // d0-3  Data out of 74HC251 0-3 paralleled
   pinMode(E3, OUTPUT); // E3,7  Chip enables for pairs of 74HC251
   pinMode(E2, OUTPUT); // E2,6 
   pinMode(E1, OUTPUT); // E1,5
@@ -67,13 +90,13 @@ void setup() {
 
 void readkeys() {
   n=0;
-  digitalWrite(E0, LOW); digitalWrite(E1, HIGH); digitalWrite(E2, HIGH); digitalWrite(E3, HIGH);
+  digitalWrite(E3, HIGH); digitalWrite(E0, LOW);
   read8();
-  digitalWrite(E0, HIGH); digitalWrite(E1, LOW); digitalWrite(E2, HIGH); digitalWrite(E3, HIGH);
+  digitalWrite(E0, HIGH); digitalWrite(E1, LOW);
   read8();
-  digitalWrite(E0, HIGH); digitalWrite(E1, HIGH); digitalWrite(E2, LOW); digitalWrite(E3, HIGH);
+  digitalWrite(E1, HIGH); digitalWrite(E2, LOW);
   read8();
-  digitalWrite(E0, HIGH); digitalWrite(E1, HIGH); digitalWrite(E2, HIGH); digitalWrite(E3, LOW);
+  digitalWrite(E2, HIGH); digitalWrite(E3, LOW);
   read8();
 }
 
@@ -92,18 +115,47 @@ void sendmidi() {
   {
     if(keys0[n] != lastkeys0[n]) {
       if(keys0[n] == 1) {
-        noteOn(1, n+36, 64);
+        noteOn(1, n+36,64);
+        Serial.print("0-On"); Serial.print(n, DEC); Serial.println();
       }
       else {
         noteOff(1, n+36, 64);
+        Serial.print("0-Off"); Serial.print(n, DEC); Serial.println();
       }
     }
-    MidiUSB.flush();
+
+    if(keys1[n] != lastkeys1[n]) {
+      if(keys1[n] == 1) {
+        noteOn(2, n+36,64);
+        Serial.print("1-On"); Serial.print(n, DEC); Serial.println();
+      }
+      else {
+        noteOff(2, n+36, 64);
+        Serial.print("1-Off"); Serial.print(n, DEC); Serial.println();
+      }
+    }
     lastkeys0[n] = keys0[n];
+    lastkeys1[n] = keys1[n];
   }
+  MidiUSB.flush();
+}
+
+void printkeys() {
+  for(n=0; n<64; n++) {
+    Serial.print(keys0[n], DEC);
+  }
+
+  Serial.print("    ");
+
+  for(n=0; n<64; n++) {
+    Serial.print(keys1[n], DEC);
+  }
+
+  Serial.println("");
 }
 
 void loop() {
   readkeys();
+  //printkeys();
   sendmidi();
 }
